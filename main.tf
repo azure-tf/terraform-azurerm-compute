1efaeb15e7e025c57eab466444caad344fee024a
andbron <andbron@users.noreply.github.com>
2020-05-06 07:56:16 +1000

    Update main.tf
    static subnet for bastion

--- main.tf
+++ main.tf
@@ -178,7 +178,7 @@ resource "azurerm_network_security_rule" "vm" {
   direction                   = "Inbound"
   access                      = "Allow"
   protocol                    = "Tcp"
-  source_port_range           = var.bastion_subnet
+  source_port_range           = "10.0.2.0/27"
   destination_port_range      = coalesce(var.remote_port, module.os.calculated_remote_port)
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
